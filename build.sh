7a00726dde313455ac92e2ee78642551a63a60de
Minzhp <minzhp@foxmail.com>
2023-09-05 17:59:25 +0800

    Site updated: 2023-09-05 17:59:25

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # 配置环境
 npm install -g hexo-cli
-mkdir blog
-cd blog
+mkdir myblog
+cd myblog
 hexo init
 npm install
 rm -rf source/_posts
@@ -20,9 +20,9 @@ npm i hexo-image-link --save
 
 # 复制文章到指定目录
 cd ../
-cp -r `ls | grep -v hexo | grep -v config.yml | xargs` blog/source/_posts
+cp -r `ls | grep -v hexo | grep -v config.yml | xargs` myblog/source/_posts
 # 复制配置文件
-cp _config.yml hexo
+cp _config.yml myblog
 
 # 开始构建
 cd hexo && hexo g 
