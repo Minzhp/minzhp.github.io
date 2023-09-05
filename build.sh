08b650d25436bb34376525021ade68cc4cd4542d
Minzhp <minzhp@foxmail.com>
2023-09-05 17:47:47 +0800

    Site updated: 2023-09-05 17:47:46

--- build.sh
+++ build.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 # 配置环境
-npm install -g hexo-cli
-mkdir hexo
-cd hexo
-hexo init
+# npm install -g hexo-cli
+# mkdir hexo
+# cd hexo
+# hexo init
 npm install
-rm -rf source/_posts
-mkdir -p source/_posts
+# rm -rf source/_posts
+# mkdir -p source/_posts
 
-# 安装主题(你们可以改成自己喜欢的主题)
-git clone https://github.com/xaoxuu/hexo-theme-stellar themes/stellar
+# # 安装主题(你们可以改成自己喜欢的主题)
+# git clone https://github.com/xaoxuu/hexo-theme-stellar themes/stellar
 
-# 使用新的渲染器
-npm un hexo-renderer-marked --save
-npm i hexo-renderer-markdown-it --save
-# 处理图片
-npm i hexo-image-link --save
+# # 使用新的渲染器
+# npm un hexo-renderer-marked --save
+# npm i hexo-renderer-markdown-it --save
+# # 处理图片
+# npm i hexo-image-link --save
 
-# 复制文章到指定目录
-cd ../
-cp -r `ls | grep -v hexo | grep -v config.yml | xargs` hexo/source/_posts
-# 复制配置文件
-cp config.yml hexo
+# # 复制文章到指定目录
+# cd ../
+# cp -r `ls | grep -v hexo | grep -v config.yml | xargs` hexo/source/_posts
+# # 复制配置文件
+# cp config.yml hexo
 
 # 开始构建
-cd hexo && hexo g 
+# cd hexo && hexo g 
+hexo g
